937cdfd6a902ffe21d9c269db2ecd2c1ca8fadec
Rodrigo Masid <remd884@mailfence.com>
2025-07-23 12:23:33 +0200

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
 		<>
 			<div className="fixed bottom-10 w-full h-10 bg-gradient-to-t from-slate-900/80 to-transparent pointer-events-none"></div>
 			<footer className="fixed bottom-0 w-full h-10 flex items-center justify-center bg-slate-800/80 backdrop-blur-md border-t border-white/10 shadow-inner">
-				<p className="text-slate-100 text-sm">r0dmd © 2025</p>
+				<p className="text-slate-100 text-sm">made with React | r0dmd © 2025</p>
 			</footer>
 		</>
 	);
